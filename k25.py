c8f28c338b4f42a5de540ba4cd1e7c51ece40d30
mollychris <123390583+mollychris@users.noreply.github.com>
2023-01-30 14:56:38 +0000

    update

--- k25.py
+++ k25.py
@@ -2,29 +2,35 @@ from stanfordkarel import *
 
 
 def main():
-    """ Karel code goes here! """
-    move()
-    move()
-    move()
-    turn_left()
-    move()
-    move()
+    """Karel code goes here!"""
     move()
     move()
     turn_left()
-    turn_left()
+    move()
+    move()
+    pick_beeper()
+    move()
     turn_left()
     move()
     turn_left()
     turn_left()
     turn_left()
     move()
-    move()
+    turn_left()
+    turn_left()
+    turn_left()
     move()
     move()
     turn_left()
-    pass
+    turn_left()
+    turn_left()
+    move()
+    move()
+    turn_left()
+     
 
+
+    pass
 
 if __name__ == "__main__":
     run_karel_program()
--- k26.py
+++ k26.py
@@ -2,13 +2,10 @@ from stanfordkarel import *
 
 
 def main():
-    """ Karel code goes here! """
-    move()
+    """Karel code goes here!"""
     move()
     move()
     turn_left()
-    move()
-    move()
     move()
     move()
     turn_left()
@@ -16,15 +13,15 @@ def main():
     turn_left()
     move()
     turn_left()
-    turn_left()
-    turn_left()
-    move()
-    move()
     move()
     move()
     turn_left()
+    turn_left()
+    turn_left()
+    move()
+    
+    
     pass
-
 
 if __name__ == "__main__":
     run_karel_program()
