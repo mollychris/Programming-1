faa05b43450432ec859432616e9492a206c5477a
mollychris <123390583+mollychris@users.noreply.github.com>
2023-01-30 15:17:39 +0000

    update

--- k26.py
+++ k26.py
@@ -19,8 +19,21 @@ def main():
     turn_left()
     turn_left()
     move()
-    
-    
+    put_beeper()
+    move()
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+    move()
+    turn_left()
+    move()
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+    move()
+    turn_left()
     pass
 
 if __name__ == "__main__":
