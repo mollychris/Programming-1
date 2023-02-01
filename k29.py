ff67825432a2170bcb2c70148324b626505ae0ab
mollychris <123390583+mollychris@users.noreply.github.com>
2023-02-01 15:32:14 +0000

    update

--- k29.py
+++ k29.py
@@ -14,6 +14,43 @@ def main():
     move()
     turn_left()
     turn_left()
+    turn_left()
+    move()
+    move()
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+    move()
+    turn_left()
+    move()
+    move()
+    turn_left()
+    move()
+    move()
+    turn_left()
+    move()
+    move()
+    turn_left()
+    move()
+    move()
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+    move()
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+    turn_left()
+    turn_left()
+    
+    
+    
+    
+  
+    
     pass
 
 if __name__ == "__main__":
