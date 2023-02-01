eb666740982d137d40a2ff9b17870bcfb3f5d02d
mollychris <123390583+mollychris@users.noreply.github.com>
2023-02-01 15:20:39 +0000

    update

---
+++ k29.py
@@ -0,0 +1,20 @@
+from stanfordkarel import *
+
+
+def main():
+    """Karel code goes here!"""
+    move()
+    move()
+    move()
+    move()
+    turn_left()
+    move()
+    move()
+    turn_left()
+    move()
+    turn_left()
+    turn_left()
+    pass
+
+if __name__ == "__main__":
+    run_karel_program()
