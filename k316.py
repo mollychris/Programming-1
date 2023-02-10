ef11ce7f26131f63a16da308706c27fb01a65170
mollychris <123390583+mollychris@users.noreply.github.com>
2023-02-10 15:25:53 +0000

    update

---
+++ k316.py
@@ -0,0 +1,46 @@
+from stanfordkarel import *
+
+
+def main():
+    """Karel code goes here!"""
+    move()
+    move()
+    turn_left()
+    move()
+    move()
+    pick_beeper()
+    move()
+    pick_beeper()
+    move()
+    pick_beeper()
+    move()
+    pick_beeper()
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+    pick_beeper()
+    move()
+    pick_beeper()
+    move()
+    pick_beeper()
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+    pick_beeper()
+    move()
+    pick_beeper()
+    move()
+    pick_beeper()
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+    pick_beeper()
+    move()
+    pick_beeper()
+    pass
+
+if __name__ == "__main__":
+    run_karel_program()
