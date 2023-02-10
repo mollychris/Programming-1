4e30132fe010c78a5bf87b4d9627e0a679359e96
mollychris <123390583+mollychris@users.noreply.github.com>
2023-02-10 14:53:56 +0000

    k31 is k311

---
+++ k31.py
@@ -0,0 +1,25 @@
+from stanfordkarel import *
+
+
+def main():
+    """Karel code goes here!"""
+    move()
+    move()
+    move()
+    move()
+    turn_left()
+    move()
+    move()
+    move()
+    move()
+    put_beeper()
+    turn_left()
+    move()
+    move()
+    move()
+    
+  
+    pass
+
+if __name__ == "__main__":
+    run_karel_program()
